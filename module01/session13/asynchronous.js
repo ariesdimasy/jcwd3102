166ebdcd24f3d26761336f6e239fba9ee2b3eae5
ariesdimasy <ariesdimasy@gmail.com>
2024-11-15 11:38:34 +0700

    update

---
+++ module01/session13/asynchronous.js
@@ -0,0 +1,5 @@
+console.log("eksekusi");
+setTimeout(function () {
+    console.log("eksekusi setelah 3 detik");
+}, 0);
+console.log(" langsung atau nunggu ?");
---
+++ module01/session13/asynchronous.ts
@@ -0,0 +1,5 @@
+console.log("eksekusi")
+setTimeout(() => {
+    console.log("eksekusi setelah 3 detik")
+},0)
+console.log(" langsung atau nunggu ?")
