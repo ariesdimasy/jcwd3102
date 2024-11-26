eba42b54a04b97b4da2886f0c37034bb2ce94c25
ariesdimasy <ariesdimasy@gmail.com>
2024-11-26 15:44:19 +0700

    add hooks

--- module02/session04/react-chakra/src/components/ListContainer/index.tsx
+++ module02/session04/react-chakra/src/components/ListContainer/index.tsx
@@ -9,21 +9,44 @@ import {
 
 } from "@chakra-ui/react"
 import style from "./ListContainer.module.css"
+import { useState } from "react"
 
 const ListContainer = () => {
 
-    const todoList = [
+    const [todo, setTodo] = useState<string>("") // input 
+    const [todoList, setTodoList] = useState([
         {title:"Membuat Content Writer untuk Astra Daihatsu ", done:false},
         {title:"Membuat Video Tiktok menggunakan Capcut untuk Iklan Purwadhika Digital Marketing", done:true},
         {title:"Membuat Content Calendar bulan desember 2024 untuk Astra Daihatsu Palembang", done:false},
         {title:"Mengajar Murid JCWD-3102 Module 02 Session 02 React Intro ", done:true}
-    ]
+    ])
 
     return (<div className={style['list-container']}>
         <Heading as={"h1"} size={"3xl"} color={'white'} margin={'60px 0 20px 0'}>TODO </Heading>
         <Group style={{width:"100%", marginBottom:'20px'}}>
             <InputAddon style={{width:"5%"}}> @ </InputAddon>
-            <Input type='text' placeholder='Create a New Todo' bgColor={'white'} />
+            <Input 
+                p={5}
+                type='text' 
+                placeholder='Create a New Todo' 
+                bgColor={'white'}
+                value={todo} // statename di taruh kedalam value
+                onChange={(e) => {
+                    setTodo(e.target.value)
+                    // todo = e.target.value
+                }}
+                
+                onKeyDown={(e) => {
+                    if(e.key == "Enter"){
+                        todoList.unshift({
+                            title:todo, // todo is state name
+                            done: false 
+                        })
+                        setTodoList(todoList) // method untuk todolist 
+                        setTodo("") // method untuk mengubah todo
+                    }
+                }}
+            />
         </Group>
         {todoList.map((item:{title:string, done:boolean}, index:number) => 
             (
@@ -34,8 +57,11 @@ const ListContainer = () => {
                 color='#494C6B' 
                 borderBottomColor={'white'}
             >
-                    
-               <Text margin={'0  0 0 20px'}>{item.title}</Text>    
+               
+               <Text margin={'0  0 0 20px'}> 
+                <input type="checkbox" checked={item.done} /> 
+                {item.title}
+               </Text>    
             </Box>
         
             </>)
